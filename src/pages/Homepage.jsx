1f805e065cdad54c0fa6be38b2e6268f0a94c5fb
Yassoori <yasserasaeed@gmail.com>
2023-07-29 21:17:56 +1200

    Type filter works

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -30,7 +30,7 @@ const Homepage = () => {
     const {setSelectedPokemon} = useContext(PokeContext)
     // useState definitions for inputs:
     const [type, setType] = useState('')
-    const [searchName, setSearchName] = useState('')
+    // const [searchTerm, setSearchTerm] = useState('')
     const [Ndex, setNdex] = useState('')
     // set state for all pokemon
     const [pokemon, setPokemon] = useState([])
@@ -38,6 +38,9 @@ const Homepage = () => {
     const [loading, setLoading] = useState(true)
     // // define useNavigate
     const navigate = useNavigate()
+    // set state for filter
+    const [filteredPokemon, setFilteredPokemon] = useState([]);
+
 
     // useEffect - API call inside
     useEffect(() => {
@@ -45,10 +48,11 @@ const Homepage = () => {
             setLoading(true)
             try {
                 const response = await axios.get(
-                    `https://pokeapi.co/api/v2/pokemon?limit=151/`
+                    // original 151
+                    // `https://pokeapi.co/api/v2/pokemon?limit=151/`
 
                     // Gen IV
-                    // `https://pokeapi.co/api/v2/pokemon?limit=493/`
+                    `https://pokeapi.co/api/v2/pokemon?limit=493/`
                     // All current
                     // `https://pokeapi.co/api/v2/pokemon?limit=1010/`
 
@@ -73,6 +77,68 @@ const Homepage = () => {
                             art: pokeResponse.data.sprites.other['official-artwork'].front_default,
                             type1: type[0],
                             type2: type[1],
+                            types: type,
+                            url: pokemon.url
+                            // ability: ability,
+                        };
+                    })
+                );
+
+                const pokemons = pokeDetails.map((pokemon) => {
+                    return {
+                        ...pokemon,
+                        onSelect: () => setSelectedPokemon(pokemon)
+                    }
+                });
+
+                console.log(response.data.results)
+
+                setPokemon(pokemons)
+                setLoading(false);
+                setFilteredPokemon(pokemons)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchPokemon()
+
+    }, [])
+
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            setLoading(true)
+            try {
+                const response = await axios.get(
+                    // Original 151
+                    // `https://pokeapi.co/api/v2/pokemon?limit=151/`
+
+                    // Gen IV
+                    `https://pokeapi.co/api/v2/pokemon?limit=493/`
+                    // All current
+                    // `https://pokeapi.co/api/v2/pokemon?limit=1010/`
+
+                );
+
+                const pokeData = response.data.results;
+                
+                // get details for pokedex entries
+                const pokeDetails = await Promise.all(
+                    pokeData.map(async (pokemon) => {
+                        const pokeResponse = await axios.get(pokemon.url)
+                        const type = pokeResponse.data.types.map(
+                            (typeData) => typeData.type.name
+                            );
+                        const sprite = pokeResponse.data.sprites.front_default;
+                        // const art = pokeResponse.data.sprites.other['official-artwork'].front_default;
+                        const pokeid = pokeResponse.data.id;
+
+                        return {
+                            name: pokemon.name,
+                            id: pokeResponse.data.id,
+                            art: pokeResponse.data.sprites.other['official-artwork'].front_default,
+                            type1: type[0],
+                            type2: type[1],
+                            types: type,
                             url: pokemon.url
                             // ability: ability,
                         };
@@ -95,29 +161,42 @@ const Homepage = () => {
             }
         }
         fetchPokemon()
-    }, [])
+
+        // Filter pokedexes based on searchTerm and type
+        const filteredData = pokemon.filter((pokemon) => {
+            // Filter based on the type
+            
+            const typeMatch = pokemon.types.includes(type.toLowerCase());
+            console.log({typeMatch});
+            return typeMatch;
+        });
+
+        // Set the filtered data to the state
+        setFilteredPokemon(filteredData);
+
+    }, [type])
 
   return (
     <div>
         <div className="type-filter">
-            <button className='type-pill , normal' value='normal'>Normal</button>
-            <button className='type-pill , fighting' value='fighting'>Fighting</button>
-            <button className='type-pill , flying' value='flying'>Flying</button>
-            <button className='type-pill , poison' value='poison'>Poison</button>
-            <button className='type-pill , fire' value='fire'>Fire</button>
-            <button className='type-pill , water' value='water'>Water</button>
-            <button className='type-pill , grass' value='grass'>Grass</button>
-            <button className='type-pill , electric' value='electric'>Electric</button>
-            <button className='type-pill , psychic' value='psychic'>Psychic</button>
-            <button className='type-pill , ice' value='ice'>Ice</button>
-            <button className='type-pill , ground' value='ground'>Ground</button>
-            <button className='type-pill , rock' value='rock'>Rock</button>
-            <button className='type-pill , bug' value='bug'>Bug</button>
-            <button className='type-pill , dragon' value='dragon'>Dragon</button>
-            <button className='type-pill , ghost' value='ghost'>Ghost</button>
-            <button className='type-pill , dark' value='dark'>Dark</button>
-            <button className='type-pill , steel' value='steel'>Steel</button>
-            <button className='type-pill , fairy' value='fairy'>Fairy</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , normal' value='normal'>Normal</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , fighting' value='fighting'>Fighting</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , flying' value='flying'>Flying</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , poison' value='poison'>Poison</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , fire' value='fire'>Fire</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , water' value='water'>Water</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , grass' value='grass'>Grass</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , electric' value='electric'>Electric</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , psychic' value='psychic'>Psychic</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , ice' value='ice'>Ice</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , ground' value='ground'>Ground</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , rock' value='rock'>Rock</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , bug' value='bug'>Bug</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , dragon' value='dragon'>Dragon</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , ghost' value='ghost'>Ghost</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , dark' value='dark'>Dark</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , steel' value='steel'>Steel</button>
+            <button onClick={(e) => setType(e.target.value)} className='type-pill , fairy' value='fairy'>Fairy</button>
         </div>
         
         {/* <h1>Pokedex</h1> */}
@@ -126,7 +205,7 @@ const Homepage = () => {
                 <Puff color="#e53131" height={100} width={100}/>
             ) : pokemon.length === 0 ? (<p>No Pokemon Found</p>) : 
             (
-               pokemon.map((item) => (
+                filteredPokemon.map((item) => (
                     <div className='pokedex-entry' key={item.name} >
                         <p className='dex-number'>National Pokédex #{item.id}</p>
                         <h2 className="name">{item.name}</h2>
