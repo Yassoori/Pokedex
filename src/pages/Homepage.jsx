31d7d2d245d5953bca40d33c4f5fc137445abf56
Yassoori <yasserasaeed@gmail.com>
2023-07-29 19:07:57 +1200

    merged

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -45,7 +45,7 @@ const Homepage = () => {
             setLoading(true)
             try {
                 const response = await axios.get(
-                    `https://pokeapi.co/api/v2/pokemon?limit=151/`
+                    `https://pokeapi.co/api/v2/pokemon?limit=493/`
                 );
 
                 const pokeData = response.data.results;
@@ -95,24 +95,24 @@ const Homepage = () => {
   return (
     <div>
         <div className="type-filter">
-            <button className='type-pill , normal'value='normal'>Normal</button>
-            <button className='type-pill , fighting'value='fighting'>Fighting</button>
-            <button className='type-pill , flying'value='flying'>Flying</button>
-            <button className='type-pill , poison'value='poison'>Poison</button>
-            <button className='type-pill , fire'value='fire'>Fire</button>
-            <button className='type-pill , water'value='water'>Water</button>
-            <button className='type-pill , grass'value='grass'>Grass</button>
-            <button className='type-pill , electric'value='electric'>Electric</button>
-            <button className='type-pill , psychic'value='psychic'>Psychic</button>
-            <button className='type-pill , ice'value='ice'>Ice</button>
-            <button className='type-pill , ground'value='ground'>Ground</button>
-            <button className='type-pill , rock'value='rock'>Rock</button>
-            <button className='type-pill , bug'value='bug'>Bug</button>
-            <button className='type-pill , dragon'value='dragon'>Dragon</button>
-            <button className='type-pill , ghost'value='ghost'>Ghost</button>
-            <button className='type-pill , dark'value='dark'>Dark</button>
-            <button className='type-pill , steel'value='steel'>Steel</button>
-            <button className='type-pill , fairy'value='fairy'>Fairy</button>
+            <button className='type-pill , normal' value='normal'>Normal</button>
+            <button className='type-pill , fighting' value='fighting'>Fighting</button>
+            <button className='type-pill , flying' value='flying'>Flying</button>
+            <button className='type-pill , poison' value='poison'>Poison</button>
+            <button className='type-pill , fire' value='fire'>Fire</button>
+            <button className='type-pill , water' value='water'>Water</button>
+            <button className='type-pill , grass' value='grass'>Grass</button>
+            <button className='type-pill , electric' value='electric'>Electric</button>
+            <button className='type-pill , psychic' value='psychic'>Psychic</button>
+            <button className='type-pill , ice' value='ice'>Ice</button>
+            <button className='type-pill , ground' value='ground'>Ground</button>
+            <button className='type-pill , rock' value='rock'>Rock</button>
+            <button className='type-pill , bug' value='bug'>Bug</button>
+            <button className='type-pill , dragon' value='dragon'>Dragon</button>
+            <button className='type-pill , ghost' value='ghost'>Ghost</button>
+            <button className='type-pill , dark' value='dark'>Dark</button>
+            <button className='type-pill , steel' value='steel'>Steel</button>
+            <button className='type-pill , fairy' value='fairy'>Fairy</button>
         </div>
         
         {/* <h1>Pokedex</h1> */}
@@ -123,6 +123,7 @@ const Homepage = () => {
             (
                pokemon.map((item) => (
                     <div className='pokedex-entry' key={item.name} >
+                        <p className='dex-number'>National Pokédex #{item.id}</p>
                         <h2 className="name">{item.name}</h2>
                         <img className='pokedex-entry-image' src={item.art} alt={item.name} />
                         <div className='type-container'>
@@ -144,7 +145,6 @@ const Homepage = () => {
                ))
             )}
         </div>
-
     </div>
   )
 }
